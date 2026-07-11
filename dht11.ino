457a06cccb77307010389201eed255d661d51b29
anupam-k22 <kvr.anupam@gmail.com>
2026-07-11 22:51:06 +0530

    Create dht11.ino

---
+++ dht11.ino
@@ -0,0 +1,24 @@
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() 
+{
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() 
+{
+  delay(2000);
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  Serial.print(temperature);
+  Serial.print(",");
+  Serial.println(humidity);
+}
